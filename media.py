e8984f853f547a5b81e38143fe748eca4596c94d
Alexey Dr <alexey.drozdov@gmail.com>
2015-10-30 23:52:35 +0300

    Initial commit

---
+++ media.py
@@ -0,0 +1,6 @@
+class Movie():
+	def __init__(self, title, story_line, poster_image, trailer_youtube):
+		self.title = title
+		self.storyline = story_line
+		self.poster_image_url = poster_image
+		self.trailer_youtube_url = trailer_youtube
